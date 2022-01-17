fcc30c144393b256f14dd027cdd11653a4a7ff3f
AngryW0lf <koslovdennis@gmail.com>
2022-01-17 12:19:51 +0200

    updating scripts

--- centos_services_check.sh
+++ centos_services_check.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+#this script will output the status of server's services
+
+NGINX='nginx.service'
+HTTPD='httpd.service'
+FTPD='pure-ftpd.service'
+PHP80='php80-php-fpm.service'
+MYSQLD='mysqld.service'
 
 GREEN="\e[1;32m" # \e = escape, [1 = bald text, ;32 = color ANSII decoding, m = finish the escape
+RED="\e[1;31m"
 NC="\e[0m" #just no color
 
-NGINX=$(systemctl is-active nginx.service)
-echo -ne "${NC}nginx.service: "
-echo -e "${GREEN}$NGINX${NC}"
+check (){
+    SERVICE=$1
+    ISACTIVE=$(systemctl is-active $SERVICE)
+    echo -ne "${NC}$SERVICE: "
+    echo -e "${GREEN}$ISACTIVE${NC}"
+}
 
-HTTPD=$(systemctl is-active httpd.service)
-echo -ne "${NC}httpd.service: "
-echo -e "${GREEN}$HTTPD${NC}"
-
-FTPD=$(systemctl is-active pure-ftpd.service)
-echo -ne "${NC}pure-ftpd.service: "
-echo -e "${GREEN}$FTPD${NC}"
-
-PHP80=$(systemctl is-active php80-php-fpm.service)
-echo -ne "${NC}php80-php-fpm.service: "
-echo -e "${GREEN}$PHP80${NC}"
-
-MYSQLD=$(systemctl is-active mysqld.service)
-echo -ne "${NC}mysqld.service: "
-echo -e "${GREEN}$MYSQLD${NC}"
+check $NGINX
+check $HTTPD
+check $FTPD
+check $PHP80
+check $MYSQLD
