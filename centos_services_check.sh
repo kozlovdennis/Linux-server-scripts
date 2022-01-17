1037a8e6dddf83aa0a8c58fe0996233bd854e7dd
AngryW0lf <koslovdennis@gmail.com>
2022-01-17 13:01:13 +0200

    updating

--- centos_services_check.sh
+++ centos_services_check.sh
@@ -11,11 +11,18 @@ GREEN="\e[1;32m" # \e = escape, [1 = bald text, ;32 = color ANSII decoding, m = 
 RED="\e[1;31m"
 NC="\e[0m" #just no color
 
+#This one checks Active status of a service, adds its name to the output and highlights it with the needed color 
 check (){
     SERVICE=$1
     ISACTIVE=$(systemctl is-active $SERVICE)
+    if [[ $ISACTIVE = 'active' ]]
+    then
+        COLOR="$GREEN"
+    else
+        COLOR="$RED"
+    fi
     echo -ne "${NC}$SERVICE: "
-    echo -e "${GREEN}$ISACTIVE${NC}"
+    echo -e "${COLOR}$ISACTIVE${NC}"
 }
 
 check $NGINX
