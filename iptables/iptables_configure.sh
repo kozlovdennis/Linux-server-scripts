73b1114798a34f5b5c25db89de8baf04ef900a8e
AngryW0lf <koslovdennis@gmail.com>
2022-02-16 16:19:01 +0200

    updating minor things in the script

--- iptables/iptables_configure.sh
+++ iptables/iptables_configure.sh
@@ -4,7 +4,7 @@ IPTABLES=/usr/sbin/iptables # which iptables
 MODPROBE=/usr/sbin/modprobe # which modprobe
 INT_NET="10.12.101.0/24" # ip addr show
 INT_NIC=enp0s8 # default router name: (to find use "arp -vn" command (you need to install net-tools for that first))
-SSH_IP="10.12.100.14" # network that SSH connection is allowed from
+SSH_IP="10.12.100.14" # an ip address that SSH connection is allowed from
 SSH_NIC=enp5s0
 
 ### flush existing rules and set chain policy setting to DROP:
