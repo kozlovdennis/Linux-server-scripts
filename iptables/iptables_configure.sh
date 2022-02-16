5744105a505de769e320aa1f5b1397cc99acc7d9
AngryW0lf <koslovdennis@gmail.com>
2022-02-16 15:03:22 +0200

    fixing a whole bunch of errors of the expired iptables syntax

--- iptables/iptables_configure.sh
+++ iptables/iptables_configure.sh
@@ -1,10 +1,11 @@
-#!/bin/zsh
-#this script configures the iptables
-IPTABLES=/usr/sbin/iptables
-MODPROBE=/usr/sbin/modprobe
-INT_NET=192.168.0.0/24
-#default router name: (to find use "arp -vn" command (you need to install net-tools for that first))
-NIC_NAME=ether
+#!/bin/bash
+# this script configures the iptables
+IPTABLES=/usr/sbin/iptables # which iptables
+MODPROBE=/usr/sbin/modprobe # which modprobe
+INT_NET="10.12.101.0/24" # ip addr show
+INT_NIC=enp0s8 # default router name: (to find use "arp -vn" command (you need to install net-tools for that first))
+SSH_IP="10.12.100.14/22" # network that SSH connection is allowed from
+SSH_NIC=enp5s0
 
 ### flush existing rules and set chain policy setting to DROP:
 echo "[+] Flushing existing iptables rules..."
@@ -15,10 +16,9 @@ $IPTABLES -P INPUT DROP
 $IPTABLES -P OUTPUT DROP
 $IPTABLES -P FORWARD DROP
 
-
 ### load connection-tracking modules
 $MODPROBE ip_conntrack
-$MODPROBE iptables_nat
+$MODPROBE iptable_nat
 $MODPROBE ip_conntrack_ftp
 $MODPROBE ip_nat_ftp
 
@@ -26,69 +26,70 @@ $MODPROBE ip_nat_ftp
 echo "[+] Setting up INPUT chain..."
 
 ### state tracking rules
-$IPTABLES -A INPUT -m state --state INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
-$IPTABLES -A INPUT -m state --state INVALID -j DROP
-$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES -A INPUT -m conntrack --ctstate INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
+$IPTABLES -A INPUT -m conntrack --ctstate INVALID -j DROP
+$IPTABLES -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 ### anti-spoofing rules
-$IPTABLES -A INPUT -i $NIC_NAME -s ! $INT_NET -j LOG --log-prefix "SPOOFED PKT "
-$IPTABLES -A INPUT -i $NIC_NAME -s ! $INT_NET -j DROP
+$IPTABLES -A INPUT -i $INT_NIC ! -s $INT_NET -j LOG --log-prefix "SPOOFED PKT "
+$IPTABLES -A INPUT -i $INT_NIC ! -s $INT_NET -j DROP
 
 ### ACCEPT rules
-$IPTABLES -A INPUT -i $NIC_NAME -p tcp -s $INT_NET --dport 22 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A INPUT -i $INT_NIC -s $INT_NET -p tcp --syn -m conntrack --ctstate NEW --dport 22 -j ACCEPT
+$IPTABLES -A INPUT -i $SSH_NIC -s $SSH_IP -p tcp --syn -m conntrack --ctstate NEW --dport 22 -j ACCEPT
 $IPTABLES -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
 ### default INPUT LOG rule
-$IPTABLES -A INPUT -i ! lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
+$IPTABLES -A INPUT ! -i lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
 
 ### OUTPUT chain ###
 echo "[+] Setting up OUTPUT chain..."
 
 ### state tracking rules
-$IPTABLES -A OUTPUT -m state --state INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
-$IPTABLES -A OUTPUT -m state --state INVALID -j DROP
-$IPTABLES -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES -A OUTPUT -m conntrack --ctstate INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
+$IPTABLES -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+$IPTABLES -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 ### ACCEPT rules for allowing connections out
-$IPTABLES -A OUTPUT -p tcp --dport 21 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 22 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 25 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 43 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 80 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 443 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 4321 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A OUTPUT -p udp --dport 53 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 21 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 22 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 25 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 43 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 80 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 443 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 4321 -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --syn -m conntrack --ctstate NEW --dport 53 -j ACCEPT
 $IPTABLES -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 
 ### default OUTPUT LOG rule
-$IPTABLES -A OUTPUT -o ! lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
+$IPTABLES -A OUTPUT ! -o lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
 
 
 ### FORWARD chain ###
 echo "[+] Setting up FORWARD chain..."
 
 ### state tracking rules
-$IPTABLES -A FORWARD -m state --state INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
-$IPTABLES -A FORWARD -m state --state INVALID -j DROP
-$IPTABLES -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES -A FORWARD -m conntrack --ctstate INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
+$IPTABLES -A FORWARD -m conntrack --ctstate INVALID -j DROP
+$IPTABLES -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 ### anti-spoofing rules
-$IPTABLES -A FORWARD -i $NIC_NAME -s ! $INT_NET -j LOG --log-prefix "SPOOFED PKT "
-$IPTABLES -A FORWARD -i $NIC_NAME -s ! $INT_NET -j DROP
+$IPTABLES -A FORWARD -i $INT_NIC ! -s $INT_NET -j LOG --log-prefix "SPOOFED PKT "
+$IPTABLES -A FORWARD -i $INT_NIC ! -s $INT_NET -j DROP
 
 ### ACCEPT rules
-$IPTABLES -A FORWARD -p tcp -i $NIC_NAME -s $INT_NET --dport 21 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p tcp -i $NIC_NAME -s $INT_NET --dport 22 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p tcp -i $NIC_NAME -s $INT_NET --dport 25 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p tcp -i $NIC_NAME -s $INT_NET --dport 43 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p tcp -i --dport 80 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p tcp -i --dport 443 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p tcp -i $NIC_NAME -s $INT_NET --dport 4321 --syn -m state --state NEW -j ACCEPT
-$IPTABLES -A FORWARD -p udp --dport 53 -m state --state NEW -j ACCEPT
+$IPTABLES -A FORWARD -i $INT_NIC -s $INT_NET -p tcp --syn -m conntrack --ctstate NEW --dport 21 -j ACCEPT
+$IPTABLES -A FORWARD -i $INT_NIC -s $INT_NET -p tcp --syn -m conntrack --ctstate NEW --dport 22 -j ACCEPT
+$IPTABLES -A FORWARD -i $INT_NIC -s $INT_NET -p tcp --syn -m conntrack --ctstate NEW --dport 25 -j ACCEPT
+$IPTABLES -A FORWARD -i $INT_NIC -s $INT_NET -p tcp --syn -m conntrack --ctstate NEW --dport 43 -j ACCEPT
+$IPTABLES -A FORWARD -p tcp --syn -m conntrack --ctstate NEW --dport 80 -j ACCEPT
+$IPTABLES -A FORWARD -p tcp --syn -m conntrack --ctstate NEW --dport 443 -j ACCEPT
+$IPTABLES -A FORWARD -i $INT_NIC -s $INT_NET -p tcp --syn -m conntrack --ctstate NEW --dport 4321 -j ACCEPT
+$IPTABLES -A FORWARD -p udp -m conntrack --ctstate NEW --dport 53 -j ACCEPT
 $IPTABLES -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
 
 ### default log rule
-$IPTABLES -A FORWARD -i ! lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
+$IPTABLES -A FORWARD ! -i lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
 
 ### NAT rules ###
 #
