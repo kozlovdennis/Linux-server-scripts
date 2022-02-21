aa0bebb74d2b6522ea1cac33b0af831930a9e005
AngryW0lf <koslovdennis@gmail.com>
2022-02-21 17:17:36 +0200

    raw

--- iptables/iptables_flush.sh
+++ iptables/iptables_flush.sh
@@ -1,21 +1,41 @@
-#!/bin/zsh
-#this script flushes the iptables to ACCEPT
+#!/bin/bash
+#this script flushes the iptables to the specified policy (ACCEPT or DROP)
 IPTABLES=/sbin/iptables
-POLICY=$1 # policy setting
+POLICY='' # policy setting
+POLICIES=( ACCEPT,DROP ) # possible policies to operate with
 
-function checkinput(){
-    if [[ $1 != 'active' ]]; then
-        COLOR="$GREEN"
-    else
-        COLOR="$RED"
+function CheckForPolicy(){
+    #if there's anything matching the array return 1
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match"]] && return 0; done
+    return 1
+}
+
+function Flush(){
+    ### flush existing rules and set chain policy settings
+    echo "[+] Flushing existing iptables rules..."
+    $IPTABLES -F
+    $IPTABLES -F -t nat
+    $IPTABLES -X
+    $IPTABLES -P INPUT $POLICY
+    $IPTABLES -P OUTPUT $POLICY
+    $IPTABLES -P FORWARD $POLICY
+}
+
+function CheckInput(){
+
+    if [[ -z "$1" ]]; then #if input is empty
+        echo "No policy is specified. Accept policy is chosen for iptables flush."
+        $POLICY='ACCEPT'
+    else #if something is in the input, check it for the keywords in array
+        if [[ CheckForPolicy "$1" "${POLICIES[@]}" == 1 ]]; then
+            $POLICY=$1
+            Flush
+        else
+            echo "Wrong policy. Try again."
+        fi
     fi
 }
 
-### flush existing rules and set chain policy setting
-echo "[+] Flushing existing iptables rules..."
-$IPTABLES -F
-$IPTABLES -F -t nat
-$IPTABLES -X
-$IPTABLES -P INPUT ACCEPT
-$IPTABLES -P OUTPUT ACCEPT
-$IPTABLES -P FORWARD ACCEPT
+CheckInput $1
